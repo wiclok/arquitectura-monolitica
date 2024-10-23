9e0c3a9706b7ef745ab9161c3f55618b8b6a2183
Cespedes Brian <cespedes.aguilera.brian@gmail.com>
2024-10-23 16:49:30 -0300

    .

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from './home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerCenter}>
+
+      </div>
+    </div>
+  );
+}
